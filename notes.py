bb8c219a183cb7f4ca31ea042a2b7163a5637731
Benjamin Johnson <bjohnson@fcusd.org>
2024-11-04 07:28:27 -0800

    Upload assignment

--- notes.py
+++ notes.py
@@ -1,15 +1,15 @@
-#Example 1: Looping through a range of numbers (for):
+#DEMO:
 
 
-
-#Example 2: Looping through a range of numbers (while):
-
-
-
-#Example 3: Looping until we get a negative number:
+printExampleHeader(1)
+#Write a function that prints out the header for each example
 
 
+printExampleHeader(2)
+#Write a function that returns the square of a number
 
-#Example 4: Looping through a string (just for fun):
 
-
+printExampleHeader(3)
+#Create a global variable
+#Write a function that does something dumb but reuse that variable name inside the function and make sure it has a different value and return that value
+#Print both to show that they are different
