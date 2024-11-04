0c2c822614379ee3fefb46a29a4afbbab7be7f54
Benjamin Johnson <bjohnson@fcusd.org>
2024-11-04 07:26:17 -0800

    Initial commit

---
+++ notes.py
@@ -0,0 +1,15 @@
+#Example 1: Looping through a range of numbers (for):
+
+
+
+#Example 2: Looping through a range of numbers (while):
+
+
+
+#Example 3: Looping until we get a negative number:
+
+
+
+#Example 4: Looping through a string (just for fun):
+
+
